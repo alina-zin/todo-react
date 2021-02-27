0b4363d47a6e2fbac5fea1e3f5f909f084f12cfa
Alina Zinchenko <c0zial00@students.oamk.fi>
2021-02-27 16:48:39 +0200

    todo3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import {useState, useEffect} from 'react';
 
-const URL = 'http://localhost/todo/retrieve.php';
+const URL = 'http://localhost/todo/';
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
 
   useEffect(() => {
     let status = 0;
-    fetch(URL)
+    fetch(URL + 'retrieve.php')
     .then(res => {
       status = parseInt(res.status);
       return res.json()
@@ -21,14 +22,53 @@ function App() {
           alert(res.error);
         }
       }, (error) => {
-        alert(error);
+        alert('Häiriö');
       }
     )
   }, [])
 
+  function save(e) {
+    e.preventDefault();
+    let status = 0;
+    fetch(URL + 'create.php', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        description: task
+      })
+    })
+    .then(res => {
+      status = parseInt(res.status);
+      return res.json()
+    })
+    .then(
+      (res) => {
+        if (status === 200) {
+          setTasks(tasks => [...tasks,res]);
+          //kenttä tyhjenee
+          setTask('');
+        } else {
+          alert(res.error);
+        }
+      }, (error) => {
+        alert('Häiriö');
+      }
+    )
+
+
+  }
+
   return (
     <div className="container">
       <h3>Todo list</h3>
+      <form onSubmit={save}>
+        <label>New task</label>
+        <input value={task} onChange={e => setTask(e.target.value)} />
+        <button>Save</button>
+      </form>
       <ol>
         {tasks.map(task => (
           <li key={task.id}>{task.description}</li>
